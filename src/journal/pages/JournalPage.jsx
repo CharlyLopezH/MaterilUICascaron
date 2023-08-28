af29155bef6734f1b5dbd9a8c1660d886d849225
Carlos Ignacio <carlosignaciolopez@gmail.com>
2023-08-28 15:26:26 -0600

    0.0

---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,17 @@
+import { MailOutline } from "@mui/icons-material";
+import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
+
+
+export const JournalPage=()=>{
+    return(
+      <JournalLayout>
+
+       <Typography variant="h5"> Suspendisse at tortor ac nisl maximus fermentum vitae id massa. Mauris mollis, erat ut viverra ultricies, ipsum nisi aliquet lacus, et sodales quam sem at lectus. 
+          Quisque tempus fermentum ligula a consequat. Aliquam suscipit tempus felis, in eleifend nisi auctor convallis. Proin hendrerit congue diam at accumsan. Fusce ut felis rhoncus dolor maximus bibendum. Praesent dui magna, varius ut convallis sed, cursus non tellus. 
+        </Typography>
+
+      </JournalLayout>
+    )
+}
+export default JournalPage;
